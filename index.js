2ab074597b93e21e7c2a4815f25a7eeb27d4c5a6
Eric Eaglstun <postpostmodern@gmail.com>
2014-02-04 14:16:16 -0600

    add handlebars, routes

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express = require( 'express' ),
+	exphbs = require( 'express3-handlebars' ),
+	logfmt = require( 'logfmt' ),
+	url = require('url'),
+	
+	app = express();
 
-app.use(logfmt.requestLogger());
+app.use( express.bodyParser() );
+app.use( logfmt.requestLogger() );
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.engine( 'handlebars', exphbs({
+	defaultLayout: 'main'
+}) );
+app.set( 'view engine', 'handlebars' );
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.get( '/', function(req, res){
+	res.render('home');
+} );
 
+app.post( '/shorten', function(req, res){
+	res.render('home');
+	
+	var long_url = req.body.url;
+	console.log( url.format(url.parse(long_url)) );
+	
+	var base = require( 'base' );
+	var base10 = '0123456789';
+	var baseconvert = 'abcd';
+	
+	x = base( 30123, base10, baseconvert );
+	console.log( x );
+	
+	x = base( x, baseconvert, base10 );
+	console.log( x );
+} );
+
+app.get( '/:id', function(req, res){
+	res.render('home');
+} );
+
+var port = Number( process.env.PORT || 5000 );
+app.listen( port, function(){
+	console.log("Listening on " + port);
+} );
