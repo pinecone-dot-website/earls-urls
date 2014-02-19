0258c3ff4c0edba814c775fbb509926513e0d820
Eric Eaglstun <postpostmodern+pinecone@gmail.com>
2014-02-19 15:06:37 -0600

    work on base conversion, postgres dependency, shorten view

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var express = require( 'express' ),
 	logfmt = require( 'logfmt' ),
 	url = require('url'),
 	
+	BASE10 = "0123456789",
+	BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	
+	pg_conn = "tcp://eaglstun:@localhost/earls_urls",
+	
 	app = express();
 
 app.use( express.bodyParser() );
@@ -18,24 +23,50 @@ app.get( '/', function(req, res){
 } );
 
 app.post( '/shorten', function(req, res){
-	res.render('home');
+	var base = require( 'base' ),
+		db_id = 0,
+		long_url = req.body.url,
+		pg = require('pg');
+		
+	var formatted_url = url.format( url.parse(long_url) );
+	console.log( 'formatted_url', formatted_url );
 	
-	var long_url = req.body.url;
-	console.log( url.format(url.parse(long_url)) );
+	pg.connect( pg_conn, function(err, client){
+		var query = client.query( 'SELECT * FROM urls WHERE url = $1 LIMIT 1', [formatted_url], function( err, result ){
+			console.log( result.rows[0].id );
+		} );
+	} );
+
+	// res = select * from urls where url = long_url limit 1
+	// if res
+	//	db_id = res.id
+	// else
+	//	timestamp = 12345
+	//	db_id = insert into urls(url,timestamp) values long_url,timestamp
+	//	
 	
-	var base = require( 'base' );
-	var base10 = '0123456789';
-	var baseconvert = 'abcd';
+	short = base( db_id, BASE10, BASE62 );
 	
-	x = base( 30123, base10, baseconvert );
-	console.log( x );
-	
-	x = base( x, baseconvert, base10 );
-	console.log( x );
+	res.render( 'shorten',{
+		short: short,
+		long_url: long_url	
+	} );
 } );
 
 app.get( '/:id', function(req, res){
-	res.render('home');
+	var base = require( 'base' ),
+		id = req.route.params.id;
+	
+	db_id = base( id, BASE62, BASE10 );
+	console.log( db_id );
+	
+	url = 'http://google.com';
+	
+	// select url from urls where id = db_id
+	// if db_id
+		res.redirect( url ); 
+	// else
+	//	res.render( 'error' );
 } );
 
 var port = Number( process.env.PORT || 5000 );
