4c5a2e2a4a86f6664637fa901dea7af2c0029c5f
Eric Eaglstun <eric@beta.io>
2017-01-04 14:37:12 -0600

    getting base-x npm module installed

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var express = require( 'express' ),
+var base_x = require( '@eaglstun/base-x' ),
+	express = require( 'express' ),
 	exphbs = require( 'express3-handlebars' ),
 	logfmt = require( 'logfmt' ),
 	pg = require( 'pg' ),
 	url = require('url'),
-	
-	BASE10 = "0123456789",
-	BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 	
 	app = express();
 
@@ -27,21 +25,22 @@ app.get( '/', function(req, res){
 } );
 
 app.post( '/shorten', function(req, res){
-	var base = require( 'base' ),
-		db_id = 0,
+	var db_id = 0,
 		long_url = req.body.url;
 		
 	var formatted_url = url.format( url.parse(long_url) );
 	console.log( 'formatted_url', formatted_url );
 
+	/*
 	pg.connect( process.env.DATABASE_URL, function(err, client, done){
 		var timestamp = new Date().getTime();
 		var query = client.query( 'INSERT INTO earls_urls (`url`, `timestamp` ) VALUES( $1, $1 )', [formatted_url, timestamp], function( err, result ){
 			console.log( 'shorten', arguments );
 		} );
 	} );
+	*/
 	
-	short = base( db_id, BASE10, BASE62 );
+	short = base_x.convert( db_id, base_x.BASE10, base_x.BASE62 );
 	
 	res.render( 'shorten',{
 		short: short,
@@ -49,12 +48,13 @@ app.post( '/shorten', function(req, res){
 	} );
 } );
 
-app.get( '/:id', function(req, res){
-	var base = require( 'base' ),
-		id = req.route.params.id;
+app.get( '/:short', function(req, res){
+	var short = req.route.params.short;
 	
-	db_id = base( id, BASE62, BASE10 );
-	
+	db_id = base_x.convert( short, base_x.BASE62, base_x.BASE10 );
+	console.log( 'db_id', db_id );
+
+	/*
 	pg.connect( process.env.DATABASE_URL, function(err, client, done){
 		var query = client.query( 'SELECT * FROM earls_urls WHERE id = $1 LIMIT 1', [db_id], function( err, result ){
 
@@ -66,6 +66,12 @@ app.get( '/:id', function(req, res){
 				res.render( 'error' );
 			}
 		} );
+	} );
+	*/
+
+	res.render( 'redirect',{
+		short: short,
+		db_id: db_id	
 	} );
 } );
 
