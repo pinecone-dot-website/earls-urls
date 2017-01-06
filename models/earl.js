898f62f3945b519996e436247e829220f7f272a2
Eric Eaglstun <postpostmodern@gmail.com>
2017-01-06 12:02:25 -0600

    initial api

--- models/earl.js
+++ models/earl.js
@@ -1,17 +1,21 @@
 "use strict";
 
-var pg = require( 'pg' );
+var base_x = require( '@eaglstun/base-x' ),
+	pg = require( 'pg' ),
+	url = url = require('url');
 
 var earl = new function(){
 
 	/**
 	*
-	*	@param int
-	*	@return string
+	*	@param int database id
+	*	@return string long url
 	*/
-	this.get = function( id, callback ){
+	this.get_by_id = function( db_id, callback ){
 		pg.connect( process.env.DATABASE_URL, function(err, client, done){
-			var query = client.query( 'SELECT * FROM urls WHERE id = $1 LIMIT 1', [id], function( err, result ){
+			var query = client.query( 'SELECT * FROM urls WHERE id = $1 LIMIT 1', [db_id], function( err, result ){
+				console.log( 'result', result );
+
 				if( !result || !result.rowCount ){
 					callback( '' );
 				}
@@ -21,6 +25,17 @@ var earl = new function(){
 				callback( row.url );
 			} );
 		} );
+	}
+
+	/**
+	*
+	*	@param string
+	*	@return
+	*/
+	this.get_by_shortid = function( earl, callback ){
+		var db_id = base_x.convert( earl, base_x.BASE75, base_x.BASE10 );
+
+		return this.get_by_id( db_id, callback );
 	}
 
 	/**
@@ -38,6 +53,38 @@ var earl = new function(){
 			    callback( row.id );
 			} );
 		} );
+	}
+
+	/**
+	*	helper function to ensure proper url 
+	*	@param string
+	*	@return string
+	*/
+	this.format = function( input_url ){
+		var parsed_url = url.parse( input_url );
+
+		if( !parsed_url.protocol )
+			parsed_url.protocol = 'http:';
+
+		if( !parsed_url.hostname ){
+			parsed_url.hostname = parsed_url.pathname;
+
+			parsed_url.path = '/';
+			parsed_url.pathname = '';
+		}
+
+		return url.format( parsed_url );
+	}
+
+	/**
+	*
+	*	@param int
+	*/
+	this.get_shortlink = function( db_id, req ){
+		console.log( 'req', req );
+		var earl = base_x.convert( db_id, base_x.BASE10, base_x.BASE75 );
+
+		return req.protocol + '://' + req.get('Host') + "/" + earl;
 	}
 }
 
