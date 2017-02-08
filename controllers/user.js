46dd394739149fa966ec4d5ccb8a7bd60e04743d
Eric Eaglstun <eric@beta.io>
2017-02-08 11:57:05 -0600

    initial list of user urls

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,6 @@
+var earl = require( '../models/earl' ),
+	user = require( '../models/user' );
+
 var controller_user = new function(){
 	this.auth = function( req, res ){
 		if( req.body.login ){
@@ -28,7 +31,25 @@ var controller_user = new function(){
 	}
 
 	this.stats = function(req, res){
-		res.render( 'user-stats' );
+		if( !res.locals.user ){
+			res.redirect( '/' );
+		}
+
+		earl.get_urls_by_user( res.locals.user.id, function(){
+
+		}, function( rows ){
+			var earls = rows.map( function(x) {
+				return {
+					short: earl.get_shortlink( x.id, req ),
+					long: x.url,
+					timestamp: x.timestamp
+				};
+			} );
+
+			res.render( 'user-stats', {
+				earls: earls
+			} );
+		} );
 	}
 }
 
