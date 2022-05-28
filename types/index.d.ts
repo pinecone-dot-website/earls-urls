134e33f6a5fc67709f231e52c7de6d53e213b507
Eric Eaglstun <postpostmodern@gmail.com>
2022-05-28 11:58:41 -0600

    fix tsconfig include for tests

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,14 @@
 export {};
+
 declare global {
   namespace Express {
     interface Request {
       flash?: Function;
+    }
+
+    interface User {
+      id: number;
+      username: string;
     }
   }
 }
