5c30d60c3c4c869978edec64f0ce51adce3dda01
Eric Eaglstun <postpostmodern@gmail.com>
2022-07-24 22:09:24 -0600

    working webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => ({
   entry: path.resolve(__dirname, './src/index.js'),
-  output:{
+  mode: env.mode || "production",
+  module: {
+    rules: [{
+      test: /\.(jsx|js)$/,
+      include: path.resolve(__dirname, 'src'),
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              "targets": "defaults"
+            }],
+            '@babel/preset-react'
+          ]
+        }
+      }]
+    }]
+  },
+  output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './public/js'),
-  }
-  // watch: argv.mode === 'development',
-};
+  },
+  watch: env.mode === 'development',
+});
